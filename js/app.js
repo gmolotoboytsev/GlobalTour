65ae61d6bff66b23f04974d62c58cde64b6d13f9
George <gmolotoboytsev>
2017-04-13 12:30:31 +0300

    New Commit

--- js/app.js
+++ js/app.js
@@ -5,5 +5,20 @@ $(function(){
         if (top > topPos) $('.floating').addClass('fixed');
         else $('.floating').removeClass('fixed');
     });
+
+
+    $('a.menu-link[href^="#"]').bind('click.smoothscroll',function (e) {
+        e.preventDefault();
+
+        var target = this.hash,
+            $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 1000, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+
 });
 
